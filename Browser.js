80f093a16f4c1e60c353cefd370d288a5def1832
Vaibhavee Singh <vaibhaveesingh89@gmail.com>
2024-02-07 21:34:39 +0530

    Add files via upload

---
+++ Browser.js
@@ -0,0 +1,12 @@
+alert("Hello World");
+
+console.log("Hello World")
+
+document.querySelector(".....")
+
+document.querySelector("....").click()
+
+document.getElementById("......")
+
+document.getElementById(".....").click()
+
---
+++ Javascript2.js
@@ -0,0 +1,5 @@
+console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+let a = 67 // a contains 67
+console.log(a)
+a = "Vaibhavee"
+console.log(a)
